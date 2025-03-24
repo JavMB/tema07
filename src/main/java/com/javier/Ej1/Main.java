c110f51e740357ecbdeb6dc4f2802f254dbd407d
JavMB <javiermengual@live.com>
2025-03-24 15:25:29 +0100

    a

--- src/main/java/com/javier/Ej1/Main.java
+++ src/main/java/com/javier/Ej1/Main.java
@@ -2,6 +2,6 @@ package com.javier.Ej1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola mundo xdasdads ");
+        System.out.println("hola mundoasd xdasdads ");
     }
 }
